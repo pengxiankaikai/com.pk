66e5d683810d1f34e533d968d01fdc95836e678f
pengkai <pkai@dacrs.com>
2016-09-30 16:33:04 +0800

    pk create

---
+++ pk-hotel/src/main/java/com/pk/hotel/PkHotelApplication.java
@@ -0,0 +1,12 @@
+package com.pk.hotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PkHotelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PkHotelApplication.class, args);
+	}
+}
